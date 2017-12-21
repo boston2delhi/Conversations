5429159137d84911bdc694e3399e0dbefdbdd447
Andrew Cunningham <wacii@mail.com>
2017-12-21 18:51:04 -0500

    Add react-scripts start support

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Root of the web running
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('conversations', () => App);
+AppRegistry.runApplication('conversations', {
+  rootTag: document.getElementById('web-root')
+});
